5aa5df2af62776a0097605be5e718eb80c248549
nimish <nimish4july1998@gmail.com>
2020-07-02 23:11:30 +0530

    added sentry for error tracking

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,92 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:newsbuster/src/notifiers/article_list_model.dart';
 import 'package:newsbuster/src/screens/app_tour/app_initial_tour.dart';
 import 'package:provider/provider.dart';
+import 'package:sentry/sentry.dart';
+import 'dsn.dart';
 
-void main() {
-  runApp(MyApp());
+
+
+/// Sentry Configuration Starts here ///
+
+// See Also : https://flutter.dev/docs/cookbook/maintenance/error-reporting
+// Example for the configuration is present at :
+// https://github.com/flutter/crashy/blob/master/lib/main.dart
+
+final SentryClient _sentry = new SentryClient(dsn: dsn);
+
+/// Whether the VM is running in debug mode.
+///
+/// This is useful to decide whether a report should be sent to sentry. Usually
+/// reports from dev mode are not very useful, as these happen on developers'
+/// workspaces rather than on users' devices in production.
+bool isInDebugMode = false;
+
+/// Reports [error] along with its [stackTrace] to Sentry.io.
+Future<Null> _reportError(dynamic error, dynamic stackTrace) async {
+    print('Caught error: $error');
+
+    // Errors thrown in development mode are unlikely to be interesting. You can
+    // check if you are running in dev mode using an assertion and omit sending
+    // the report.
+    if (isInDebugMode) {
+        print(stackTrace);
+        print('In dev mode. Not sending report to Sentry.io.');
+        return;
+    }
+
+    print('Reporting to Sentry.io...');
+
+    final SentryResponse response = await _sentry.captureException(
+        exception: error,
+        stackTrace: stackTrace,
+    );
+
+    if (response.isSuccessful) {
+        print('Success! Event ID: ${response.eventId}');
+    } else {
+        print('Failed to report to Sentry.io: ${response.error}');
+    }
 }
+
+Future<Null> main() async {
+    // This captures errors reported by the Flutter framework.
+    FlutterError.onError = (FlutterErrorDetails details) async {
+        if (isInDebugMode) {
+            // In development mode simply print to console.
+            FlutterError.dumpErrorToConsole(details);
+        } else {
+            // In production mode report to the application zone to report to
+            // Sentry.
+            Zone.current.handleUncaughtError(details.exception, details.stack);
+        }
+    };
+
+    // This creates a [Zone] that contains the Flutter application and stablishes
+    // an error handler that captures errors and reports them.
+    //
+    // Using a zone makes sure that as many errors as possible are captured,
+    // including those thrown from [Timer]s, microtasks, I/O, and those forwarded
+    // from the `FlutterError` handler.
+    //
+    // More about zones:
+    //
+    // - https://api.dartlang.org/stable/1.24.2/dart-async/Zone-class.html
+    // - https://www.dartlang.org/articles/libraries/zones
+    runZonedGuarded<Future<Null>>(() async {
+        runApp(new MyApp());
+    }, (error, stackTrace) async {
+        await _reportError(error, stackTrace);
+    });
+}
+
+/// Sentry Configuration Ended here ///
+
+
+
+
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
